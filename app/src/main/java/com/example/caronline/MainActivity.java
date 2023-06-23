b56022be7a2e7794c238beb0ec4fc1e27cb24f92
Drtaaluma <drtaaluma3236@gmail.com>
2023-06-23 04:57:20 +0300

    Car Project

--- app/src/main/java/com/example/caronline/MainActivity.java
+++ app/src/main/java/com/example/caronline/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String[] carList ={"r","rl2","c","b"};
+    int[] carImage ={R.drawable.r,R.drawable.rl2,R.drawable.c,R.drawable.b};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
