93974d2ca4ac547cad5e9427807fadc35d044f5a
Drtaaluma <drtaaluma3236@gmail.com>
2023-06-24 12:22:13 +0300

    Car Project

--- app/src/main/java/com/example/caronline/loginActivity.java
+++ app/src/main/java/com/example/caronline/loginActivity.java
@@ -1,13 +1,31 @@
 package com.example.caronline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button =(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCarList();
+
+            }
+        });
     }
+    public void openCarList(){
+        Intent intent = new Intent(this,CarList.class);
+        startActivity(intent);
+    }
+
 }
