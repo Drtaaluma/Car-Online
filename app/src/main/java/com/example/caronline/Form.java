79b17086b6a6e0187b67dd6d35eabe6df08ab1df
Drtaaluma <drtaaluma3236@gmail.com>
2023-07-05 19:58:20 +0300

    Car Project

--- app/src/main/java/com/example/caronline/Form.java
+++ app/src/main/java/com/example/caronline/Form.java
@@ -1,13 +1,29 @@
 package com.example.caronline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Form extends AppCompatActivity {
+    private Button submit2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        submit2 =(Button) findViewById(R.id.submit2);
+        submit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),CarList.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/caronline/MainActivity.java
+++ app/src/main/java/com/example/caronline/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,loginActivity.class);
+                Intent intent = new Intent(MainActivity.this,CarList.class);
                 startActivity(intent);
                 finish();
             }
